b05bb7d586bc03d33719a7f5c669574737e46cd4
mcsavvy <phishink5@gmail.com>
2023-04-23 12:52:55 +0100

    Added ReadMe

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,12 @@ Simple Hello World Flask App
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     """Website index page"""
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
